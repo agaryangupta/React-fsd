897da02af39b915ee0ea96f9e6689cde947cef02
agaryangupta <agaryangupta123@gmail.com>
2025-05-28 13:20:40 +0530

    done

---
+++ src/component/navbar.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import propTypes from 'prop-types'
+
+
+export default function Navbar(props) {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#"></a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">{props.title}</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">{props.aboutText}</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"/></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      {props.searchBar?<form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>:"No Search Bar"}
+    </div>
+  </div>
+</nav>
+    </div>
+  )
+}
+
+Navbar.propTypes = {
+    title : propTypes.string,
+    aboutText : propTypes.string
+}
+
+Navbar.defaultProps = {
+    title : "Set title here",
+    aboutText : "Set about text here",
+    SearchBar : false
+}
